24a8ff78e61e231ba9f1fb4aeda9bddbbf34bb14
b-s-code <89502375+b-s-code@users.noreply.github.com>
2023-04-12 12:24:06 +0800

    camelCase

--- js-geo-animation/script.js
+++ js-geo-animation/script.js
@@ -3,8 +3,8 @@ const redDotStyle = document.getElementById("redDot").style;
 const blueDotStyle = document.getElementById("blueDot").style;
 
 var angle = 0; // radians
-var redoffset = Math.PI / 2;
-var blueoffset = 3 * Math.PI / 2;
+var redOffset = Math.PI / 2;
+var blueOffset = 3 * Math.PI / 2;
 var hOffsetShared = 4;
 var vOffsetShared = 3;
 
@@ -15,9 +15,9 @@ function UpdatePositions()
     // bottom means y
     greenDotStyle.left = Math.cos(angle) + hOffsetShared + "cm";
     greenDotStyle.bottom = Math.sin(angle) + vOffsetShared + "cm";
-    redDotStyle.left = Math.cos(angle + redoffset) + hOffsetShared + "cm";
-    redDotStyle.bottom = Math.cos(angle + redoffset) + vOffsetShared + "cm";
-    blueDotStyle.left = Math.sin(angle + blueoffset) + hOffsetShared + "cm";
-    blueDotStyle.bottom = Math.sin(angle + blueoffset) + vOffsetShared + "cm";
+    redDotStyle.left = Math.cos(angle + redOffset) + hOffsetShared + "cm";
+    redDotStyle.bottom = Math.cos(angle + redOffset) + vOffsetShared + "cm";
+    blueDotStyle.left = Math.sin(angle + blueOffset) + hOffsetShared + "cm";
+    blueDotStyle.bottom = Math.sin(angle + blueOffset) + vOffsetShared + "cm";
     angle += 0.008; // works nice enough
 }
